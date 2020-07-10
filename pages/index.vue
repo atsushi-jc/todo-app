b1f4bfcc0128fb89a18b461c0b3967bf818f19ed
nisiki009 <nisiki009@gmail.com>
2020-07-10 19:08:22 +0900

    no message

--- pages/index.vue
+++ pages/index.vue
@@ -2,55 +2,80 @@
   <section class="container">
     <h1>Todoリスト</h1>
     <div class="addArea">
-      <input type="text" name="addName" id="addName" placeholder="タスクを入力してください">
+      <input type="text" name="addName" id="addName"/>
       <button id="addButton" class="button button--green">追加</button>
     </div>
+    <table class="Lists">
+      <thead>
+        <tr>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in todos" :key="index">
+          <td>{{ item.content }}</td>
+          <td>{{ item.created }}</td>
+          <td>
+            <button class="button">{{ item.state }}</button>
+          </td>
+          <td>
+            <button class="button button--red">削除</button>
+          </td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+    <table class="footer">
+      <td>
+        <div>
+          <button class="button button--Save">保存</button>
+        </div>
+      </td>
+    </table>
   </section>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
+import Vuex from "vuex";
+import {mapState} from 'vuex';
 
-export default Vue.extend({})
+export default {
+  data: function() {
+    return {
+      content: ''
+    }
+  },
+  computed: {
+    ...mapState(['todos'])
+  }
+}
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: () => ({
+      todos: [
+        { content: "テスト", created: "2020-04-30 17:00", state: "作業前" },
+        {
+          content: "コーディング",
+          created: "2020-04-30 16:00",
+          state: "作業中"
+        },
+        { content: "環境構築", created: "2020-04-30 15:30", state: "完了" }
+      ]
+    })
+  });
+};
+
+var app = new Vue({
+  el: "#addArea",
+  data: {
+    todos: []
+  },
+  methods: {
+    addTodo: function() {}
+  }
+});
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;1
-  text-align: center;
-}
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
