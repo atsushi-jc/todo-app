255e975430e8fc00f461e69ba9f4eda919996048
nisiki009 <nisiki009@gmail.com>
2020-07-10 15:13:58 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <section class="container">
+    <h1>Todoリスト</h1>
+    <div class="addArea">
+      <input type="text" name="addName" id="addName" placeholder="タスクを入力してください">
+      <button id="addButton" class="button button--green">追加</button>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;1
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
