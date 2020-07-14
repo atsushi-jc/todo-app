11e4610d041be00f604bcb40585ff1068d3b3028
nisiki009 <nisiki009@gmail.com>
2020-07-15 08:16:03 +0900

    ・index.tsに修正。 ・削除ボタンの挙動実装

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
   <section class="container">
     <h1>Todoリスト</h1>
     <div class="addArea">
-      <input type="text" name="addName"  v-model="content" id="addName"/>
-      <button id="addButton" class="button" v-on:click="insert">追加</button>
+      <input type="text" name="addName" v-model="content" id="addName" />
+      <button id="addButton" class="button" v-on:click="insert()">追加</button>
     </div>
     <table class="Lists">
       <thead>
@@ -13,49 +13,55 @@
       </thead>
       <tbody>
         <div>
-        <tr v-for="(item, index) in todos" :key="index">
-          <td>{{ item.content }}</td>
-          <td>{{ item.created }}</td>
-          <td><button class="button">{{ item.state }}</button></td>
-          <td><button class="button">編集</button></td>
-          <td><button class="button">削除</button></td>
-        </tr>
+          <tr v-for="(item, index) in todos" :key="index">
+            <td>{{ item.content }}</td>
+            <td>{{ item.created }}</td>
+            <td>
+              <button class="button">{{ item.state }}</button>
+            </td>
+            <td>
+              <button class="button">編集</button>
+            </td>
+            <td>
+              <button class="button" v-on:click="remove(todo)">削除</button>
+            </td>
+          </tr>
         </div>
       </tbody>
     </table>
-        <div>
-          <button id="save" class="button">保存</button>
-        </div>
+    <div>
+      <button id="save" class="button">保存</button>
+    </div>
   </section>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Vuex from "vuex";
-import {mapState} from 'vuex';
+import { mapState } from "vuex";
 
 export default {
   data: function() {
     return {
-      content: ''
-    }
+      content:  "",
+    };
   },
   computed: {
-    ...mapState(['todos'])
+    ...mapState(["todos"])
   },
 
   methods: {
-  insert: function() {
-    if(this.content != ''){
-      this.$store.commit('insert', {content: this.content});
-      this.content = '';
+    insert: function() {
+      if (this.content !== "") {
+        this.$store.commit("insert", { content: this.content });
+        this.content = "";
+      }
+    },
+    remove: function(todo:any) {
+      this.$store.commit("remove", todo);
     }
   }
-}
-}
-
-
-
+};
 </script>
 
 
---
+++ store/index.ts
@@ -0,0 +1,35 @@
+import Vuex from 'vuex';
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: () => ({
+      todos: []
+    }),
+    mutations: {
+      insert: function(state:any , obj:any) {
+        var d = new Date();
+        var fmt = d.getFullYear()
+                + '-' + ('00' + (d.getMonth() + 1)).slice(-2)
+                + '-' + ('00' + d.getDate()).slice(-2)
+                + ' ' + ('00' + d.getHours()).slice(-2)
+                + ':' + ('00' + d.getMinutes()).slice(-2);
+        state.todos.unshift({
+          content: obj.content,
+          created: fmt,
+          state: '作業中',
+        });
+      },
+      remove: function(state:any) {
+        for(let i = 0; i < state.todos.length; i++) {
+            const ob = state.todos[i];
+                state.todos.splice(i, 1);
+                return;
+        }
+    }
+    }
+  })
+}
+
+
+
+export default createStore;
